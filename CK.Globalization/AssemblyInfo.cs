476f8ee4149a08a9600d7a44816ef5ca1103bab1
Olivier Spinelli <olivier.spinelli@signature-code.com>
2023-08-13 13:06:06 +0200

    Forgot the IsModelDependent assembly attribute.

---
+++ CK.Globalization/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+
+//
+// This enables the CK.StObj automatic DI to analyze this assembly: the ExtendedCultureInfo ubiquitous endpoint scoped service,
+// the singleton TranslationService and the CurrentCultureInfo scoped service.
+[assembly: CK.Setup.IsModelDependent()]
