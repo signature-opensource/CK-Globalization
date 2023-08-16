fbf049364e96d0bb918d8bb5200a2bb72808a6f4
Olivier Spinelli <olivier.spinelli@signature-code.com>
2023-08-16 18:17:23 +0200

    Added collector version for GetUserMessages.

--- CK.Globalization/UserMessage/UserMessageExceptionExtensions.cs
+++ CK.Globalization/UserMessage/UserMessageExceptionExtensions.cs
@@ -20,46 +20,72 @@ namespace CK.Core
         /// <returns>A list of one or more messages.</returns>
         public static List<UserMessage> GetUserMessages( this Exception ex, CurrentCultureInfo culture )
         {
+            Throw.CheckNotNullArgument( culture != null );
             List<UserMessage> messages = new List<UserMessage>();
-            var e = ex;
-            Collect( messages, 0, e, null, culture );
+            Collect( messages.Add, 0, ex, null, culture );
             return messages;
         }
 
         /// <inheritdoc cref="GetUserMessages(Exception, CurrentCultureInfo)"/>
         public static List<UserMessage> GetUserMessages( this Exception ex, ExtendedCultureInfo culture )
         {
+            Throw.CheckNotNullArgument( culture != null );
             List<UserMessage> messages = new List<UserMessage>();
-            var e = ex;
-            Collect( messages, 0, e, culture, null );
+            Collect( messages.Add, 0, ex, culture, null );
             return messages;
         }
 
-        static void Collect( List<UserMessage> messages, byte depth, Exception e, ExtendedCultureInfo? culture, CurrentCultureInfo? current )
+        /// <summary>
+        /// Gets a list of error user messages with the messages of this exception and
+        /// any <see cref="Exception.InnerException"/> or <see cref="AggregateException.InnerExceptions"/>.
+        /// <para>
+        /// Inner exceptions are indented by <see cref="UserMessage.Depth"/>.
+        /// </para>
+        /// </summary>
+        /// <param name="ex">The exception.</param>
+        /// <param name="culture">The current culture.</param>
+        /// <param name="collector">Message collector.</param>
+        /// <returns>A list of one or more messages.</returns>
+        public static void GetUserMessages( this Exception ex, CurrentCultureInfo culture, Action<UserMessage> collector )
+        {
+            Throw.CheckNotNullArgument( culture != null );
+            Throw.CheckNotNullArgument( collector );
+            Collect( collector, 0, ex, null, culture );
+        }
+
+        /// <inheritdoc cref="GetUserMessages(Exception, CurrentCultureInfo, Action{UserMessage})"/>
+        public static void GetUserMessages( this Exception ex, ExtendedCultureInfo culture, Action<UserMessage> collector)
+        {
+            Throw.CheckNotNullArgument( culture != null );
+            Throw.CheckNotNullArgument( collector );
+            Collect( collector, 0, ex, culture, null );
+        }
+
+        static void Collect( Action<UserMessage> collector, byte depth, Exception e, ExtendedCultureInfo? culture, CurrentCultureInfo? current )
         {
             if( e is MCException mC )
             {
-                messages.Add( mC.AsUserMessage().With( depth ) );
-                if( mC.InnerException != null ) Collect( messages, ++depth, mC.InnerException, culture, current );
+                collector( mC.AsUserMessage().With( depth ) );
+                if( mC.InnerException != null ) Collect( collector, ++depth, mC.InnerException, culture, current );
             }
             else if( e is AggregateException a )
             {
-                AddUserMessage( messages, depth++, culture, current, a.Message );
-                foreach( var sub in a.InnerExceptions ) Collect( messages, depth, sub, culture, current );
+                AddUserMessage( collector, depth++, culture, current, a.Message );
+                foreach( var sub in a.InnerExceptions ) Collect( collector, depth, sub, culture, current );
             }
             else
             {
-                AddUserMessage( messages, depth, culture, current, e.Message );
-                if( e.InnerException != null ) Collect( messages, ++depth, e.InnerException, culture, current );
+                AddUserMessage( collector, depth, culture, current, e.Message );
+                if( e.InnerException != null ) Collect( collector, ++depth, e.InnerException, culture, current );
             }
 
-            static void AddUserMessage( List<UserMessage> messages, byte depth, ExtendedCultureInfo? culture, CurrentCultureInfo? current, string text )
+            static void AddUserMessage( Action<UserMessage> collector, byte depth, ExtendedCultureInfo? culture, CurrentCultureInfo? current, string text )
             {
-                if( culture != null ) messages.Add( UserMessage.Error( culture, text ).With( depth ) );
+                if( culture != null ) collector( UserMessage.Error( culture, text ).With( depth ) );
                 else
                 {
                     Throw.DebugAssert( current != null );
-                    messages.Add( UserMessage.Error( current, text ).With( depth ) );
+                    collector( UserMessage.Error( current, text ).With( depth ) );
                 }
             }
         }
