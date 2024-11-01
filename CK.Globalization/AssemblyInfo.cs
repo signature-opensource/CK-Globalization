a494286d4dba3ae277f2075c2364d221c27501cc
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-11-01 12:26:08 +0100

    Added CK.Abstractions dependencies.

--- CK.Globalization/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-
-//
-// This enables the CK.StObj automatic DI to analyze this assembly: the ExtendedCultureInfo ubiquitous endpoint scoped service,
-// the singleton TranslationService and scoped CurrentCultureInfo services.
-[assembly: CK.Setup.IsModelDependent()]
